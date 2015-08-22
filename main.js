46d8737afd1b4d3dc8735274a9f90e1904afee38
rkoeninger <rkoeninger@att.net>
2015-08-22 16:49:33 -0400

    Added empty spec test, spec page, changed jslint indent setting

--- main.js
+++ main.js
@@ -5,7 +5,7 @@
  * abd use it to populate the page content.
  */
 
-/*jslint browser: true, regexp: true*/
+/*jslint browser: true, regexp: true, indent: 4*/
 /*global marked, hljs*/
 
 var defaultArticle = "underConstruction";
@@ -14,122 +14,122 @@ var historyUrlBase = "//github.com/rkoeninger/rkoeninger.github.io/commits/maste
 var commitsUrlBase = "//api.github.com/repos/rkoeninger/rkoeninger.github.io/commits?path=";
 
 function queryString(key) {
-  var qsParts, argAndVal, i;
+    var qsParts, argAndVal, i;
 
-  qsParts = window.location.search.substr(1).split("&");
+    qsParts = window.location.search.substr(1).split("&");
 
-  if (!(qsParts || key)) {
-    return "";
-  }
-
-  for (i = 0; i < qsParts.length; ++i) {
-    argAndVal = qsParts[i].split("=");
-
-    if (argAndVal.length === 2 && argAndVal[0] === key) {
-      return decodeURIComponent(argAndVal[1].replace(/\+/g, " "));
+    if (!(qsParts || key)) {
+        return "";
     }
-  }
 
-  return "";
+    for (i = 0; i < qsParts.length; ++i) {
+        argAndVal = qsParts[i].split("=");
+
+        if (argAndVal.length === 2 && argAndVal[0] === key) {
+            return decodeURIComponent(argAndVal[1].replace(/\+/g, " "));
+        }
+    }
+
+    return "";
 }
 
 function endsWith(str, suffix) {
-  return str.indexOf(suffix, str.length - suffix.length) !== -1;
+    return str.indexOf(suffix, str.length - suffix.length) !== -1;
 }
 
 function getArticleFileName() {
-  var articleId = queryString("articleId");
+    var articleId = queryString("articleId");
 
-  if (!articleId) {
-    articleId = defaultArticle;
-  }
+    if (!articleId) {
+        articleId = defaultArticle;
+    }
 
-  if (!endsWith(articleId, ".md")) {
-    articleId += ".md";
-  }
+    if (!endsWith(articleId, ".md")) {
+        articleId += ".md";
+    }
 
-  return articleId;
+    return articleId;
 }
 
 function getArticleUrl() {
-  return "/articles/" + getArticleFileName();
+    return "/articles/" + getArticleFileName();
 }
 
 function setSourceLink() {
-  var sourceLink = $("#source-link");
-  sourceLink.attr("href", getArticleUrl());
+    var sourceLink = $("#source-link");
+    sourceLink.attr("href", getArticleUrl());
 }
 
 function setHistoryLink() {
-  var historyLink = $("#history-link");
-  historyLink.attr("href", historyUrlBase + getArticleFileName());
+    var historyLink = $("#history-link");
+    historyLink.attr("href", historyUrlBase + getArticleFileName());
 }
 
 function hideDisqusThreadMaybe(articleUrl, disqusDiv) {
-  var i;
-  for (i = 0; i < disqusExcludedArticles.length; ++i) {
-    if (endsWith(articleUrl, disqusExcludedArticles[i])) {
-      disqusDiv.hide();
-      return;
+    var i;
+    for (i = 0; i < disqusExcludedArticles.length; ++i) {
+        if (endsWith(articleUrl, disqusExcludedArticles[i])) {
+            disqusDiv.hide();
+            return;
+        }
     }
-  }
 }
 
 function getCommitHistoryUrl() {
-  return commitsUrlBase + getArticleUrl();
+    return commitsUrlBase + getArticleUrl();
 }
 
 function setLastModifiedLabel(label) {
-  var lastModifiedLabel = $("#last-modified");
-  lastModifiedLabel.text(label);
+    var lastModifiedLabel = $("#last-modified");
+    lastModifiedLabel.text(label);
 }
 
 $(function () {
-  var articleUrl, articleDiv, disqusDiv;
+    var articleUrl, articleDiv, disqusDiv;
 
-  articleDiv = $("#article0");
-  disqusDiv = $("#disqus_thread");
+    articleDiv = $("#article0");
+    disqusDiv = $("#disqus_thread");
 
-  setSourceLink();
-  setHistoryLink();
+    setSourceLink();
+    setHistoryLink();
 
-  articleUrl = getArticleUrl();
+    articleUrl = getArticleUrl();
 
-  $.ajax({
-    type: "GET",
-    dataType: "text",
-    url: articleUrl,
-    success: function (articleMarkdown) {
-      var titleMatches;
+    $.ajax({
+        type: "GET",
+        dataType: "text",
+        url: articleUrl,
+        success: function (articleMarkdown) {
+            var titleMatches;
 
-      articleDiv.html(marked(articleMarkdown));
-      hideDisqusThreadMaybe(articleUrl, disqusDiv);
+            articleDiv.html(marked(articleMarkdown));
+            hideDisqusThreadMaybe(articleUrl, disqusDiv);
 
-      // find (#) elements in markdown
-      titleMatches = articleMarkdown.match(/\x23\x20(.*)\x0D?\x0A/);
+            // find (#) elements in markdown
+            titleMatches = articleMarkdown.match(/\x23\x20(.*)\x0D?\x0A/);
 
-      if (titleMatches) {
-        // strip markdown formatting from (#) element and make it the title
-        document.title = $(marked(titleMatches[1]))[0].textContent;
-      }
+            if (titleMatches) {
+                // strip markdown formatting from (#) element and make it the title
+                document.title = $(marked(titleMatches[1]))[0].textContent;
+            }
 
-      hljs.initHighlighting();
-    },
-    error: function (jqXHR, textStatus, errorThrown) {
-      articleDiv.html("failed to load article content<br />" + textStatus + "<br />" + errorThrown);
-    }
-  });
+            hljs.initHighlighting();
+        },
+        error: function (ignore, textStatus, errorThrown) {
+            articleDiv.html("failed to load article content<br />" + textStatus + "<br />" + errorThrown);
+        }
+    });
 
-  $.getJSON(
-    getCommitHistoryUrl(),
-    function (data) {
-      var author, date;
+    $.getJSON(
+        getCommitHistoryUrl(),
+        function (data) {
+            var author, date;
 
-      if (data.length > 0) {
-        author = data[0].commit.author.name;
-        date = new Date(data[0].commit.author.date).toLocaleDateString();
-        setLastModifiedLabel("Last modified by " + author + " on " + date);
-      }
-    }
-  );
+            if (data.length > 0) {
+                author = data[0].commit.author.name;
+                date = new Date(data[0].commit.author.date).toLocaleDateString();
+                setLastModifiedLabel("Last modified by " + author + " on " + date);
+            }
+        }
+    );
 });
---
+++ spec.js
@@ -0,0 +1,8 @@
+/*jslint indent: 4*/
+/*global describe, it*/
+
+describe("", function () {
+    it("", function () {
+        return;
+    });
+});
