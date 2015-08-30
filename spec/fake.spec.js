018cabb64441d22ae69d736d737f184832b65597
rkoeninger <rkoeninger@att.net>
2015-08-29 22:58:40 -0400

    Removed dummy specs

--- spec/fake.spec.js
+++
@@ -1,7 +0,0 @@
-define([], function () {
-	describe("fake", function () {
-		it("should be true", function () {
-			expect(true).toBe(true);
-		});
-	});
-});
--- spec/nonrequire.spec.js
+++
@@ -1,5 +0,0 @@
-	describe("nonrequire", function () {
-		it("should be true", function () {
-			expect(true).toBe(true);
-		});
-	});
