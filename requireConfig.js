adee5322333e3329ef951db8844ba17e7a653303
rkoeninger <rkoeninger@att.net>
2015-08-30 11:54:21 -0400

    Now loading jasmine from cdn

--- requireConfig.js
+++ requireConfig.js
@@ -6,6 +6,8 @@
 require.config({
     paths: {
         "jquery": "//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min",
+        "main": "/main",
+        "marked": "/marked"
     }
 });
 
--- requireTestConfig.js
+++ requireTestConfig.js
@@ -6,9 +6,9 @@
 require.config({
     paths: {
         "jquery": "//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min",
-        "jasmine": "/jasmine/jasmine",
-        "jasmine-html": "/jasmine/jasmine-html",
-        "jasmine-boot": "/jasmine/boot",
+        "jasmine": "//cdnjs.cloudflare.com/ajax/libs/jasmine/2.3.4/jasmine.min",
+        "jasmine-html": "//cdnjs.cloudflare.com/ajax/libs/jasmine/2.3.4/jasmine-html.min",
+        "jasmine-boot": "//cdnjs.cloudflare.com/ajax/libs/jasmine/2.3.4/boot.min",
         "main-spec": "/spec/main.spec",
         "main": "/main",
         "marked": "/marked"
