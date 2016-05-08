ca9da2f8957795f6a330e9e16b7fffe855ff7d0b
rkoeninger <rkoeninger@att.net>
2016-05-08 13:26:51 -0400

    Added article about windows cmd files
    Updated highlight.js version

--- spec.config.js
+++ spec.config.js
@@ -13,7 +13,7 @@ require.config({
     "webcomponents": "//cdnjs.cloudflare.com/ajax/libs/webcomponentsjs/0.7.18/webcomponents-lite.min",
     "marked": "//cdnjs.cloudflare.com/ajax/libs/marked/0.3.2/marked.min",
     "mathjax": "//cdn.mathjax.org/mathjax/latest/MathJax.js?config=TeX-AMS_HTML&amp;delayStartupUntil=configured",
-    "hljs": "//cdnjs.cloudflare.com/ajax/libs/highlight.js/8.3/highlight.min",
+    "hljs": "//cdnjs.cloudflare.com/ajax/libs/highlight.js/9.3.0/highlight.min",
     "main": "/main",
     "main-spec": "/spec/main.spec"
   },
