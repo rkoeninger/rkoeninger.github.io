d2723e183a150321235b11aaa085f652aefab43c
rkoeninger <rkoeninger@att.net>
2016-05-18 22:24:45 -0400

    Added custom highlight.pack (containing everything)

--- main.config.js
+++ main.config.js
@@ -10,7 +10,7 @@ require.config({
     "webcomponents": "//cdnjs.cloudflare.com/ajax/libs/webcomponentsjs/0.7.18/webcomponents-lite.min",
     "marked": "//cdnjs.cloudflare.com/ajax/libs/marked/0.3.2/marked.min",
     "mathjax": "//cdn.mathjax.org/mathjax/latest/MathJax.js?config=TeX-AMS_HTML&amp;delayStartupUntil=configured",
-    "hljs": "//cdnjs.cloudflare.com/ajax/libs/highlight.js/9.3.0/highlight.min",
+    "hljs": "/highlight.pack",
     "main": "/main"
   },
   shim: {
