919360fbc70195b4de33576f4c82008472b162de
rkoeninger <rkoeninger@att.net>
2016-05-19 00:53:03 -0400

    Introduce nomobile and onlymobile block-level tags
    Made more use of arrow functions

--- main.js
+++ main.js
@@ -3,8 +3,8 @@
 /*jslint browser: true, regexp: true, nomen: true, indent: 2*/
 /*global define, MathJax*/
 
-define(["marked", "jquery", "mathjax", "hljs", "lodash"], function (marked, $, ignore, hljs, _) {
-  var main = (function () {
+define(["marked", "jquery", "mathjax", "hljs", "lodash"], (marked, $, ignore, hljs, _) => {
+  var main = (() => {
     var defaultArticle = "default.md",
       disqusEnabled = false,
       disqusExcludedArticles = ["/default.md", "/status.md", "/xmlTest.xml"],
@@ -28,7 +28,7 @@ define(["marked", "jquery", "mathjax", "hljs", "lodash"], function (marked, $, i
         return "";
       }
 
-      argVal = _.find(qsParts, function (qsPart) {
+      argVal = _.find(qsParts, qsPart => {
         var argValSplit = qsPart.split("=");
         return argValSplit.length === 2 && argValSplit[0] === key;
       });
@@ -73,13 +73,22 @@ define(["marked", "jquery", "mathjax", "hljs", "lodash"], function (marked, $, i
     }
 
     function getPageTitle(articleHtml) {
-      return $(_.find($(articleHtml), function (x) { return $(x).is("h1"); })).text() || "Fear of a Blue Screen";
+      return $(_.find($(articleHtml), x => $(x).is("h1"))).text() || "Fear of a Blue Screen";
     }
 
     function parseArticleId(url) {
       var parser = document.createElement("a");
       parser.href = url;
       return defaultExtension(getArticleFileName(parser.search));
+    }
+
+    function getDisplayType(element) {
+      var cStyle = element.currentStyle || window.getComputedStyle(element, ""); 
+      return cStyle.display;
+    }
+
+    function isInlineElement(element) {
+      return getDisplayType(element) === "inline";
     }
 
     function newClearFix() {
@@ -92,11 +101,11 @@ define(["marked", "jquery", "mathjax", "hljs", "lodash"], function (marked, $, i
       return document.createTextNode(text);
     }
 
-    function newElement(name, children) {
+    function newElement(name, children, classes, attributes) {
       var element = document.createElement(name);
-      _.forEach(children, function (child) {
-        element.appendChild(child);
-      });
+      _.forEach(children, x => element.appendChild(x));
+      _.forEach(classes, x => element.classList.add(x));
+      _.forEach(attributes, (v, k) => element.setAttribute(k, v));
       return element;
     }
 
@@ -105,32 +114,34 @@ define(["marked", "jquery", "mathjax", "hljs", "lodash"], function (marked, $, i
 
     function codeHandler(lang, ext, name, xml) {
       var text = _.trim(xml.textContent);
-      var codeElement = newElement("code", [newText(text)]);
+      var codeElement = newElement("code", [newText(text)], ["lang-" + lang]);
       var preElement = newElement("pre", [codeElement]);
-      codeElement.classList.add("lang-" + lang);
-      var langIcon = newElement("img", []);
-      langIcon.setAttribute("src", "/logos/" + lang + ".svg");
-      langIcon.setAttribute("title", name);
-      langIcon.setAttribute("alt", name);
-      langIcon.classList.add("lang-icon");
-      var fileName = newElement("span", [newText("file." + ext)]);
-      fileName.classList.add("file-name");
+      var langIcon = newElement("img", [], ["lang-icon"], {
+        "src": "/logos/" + lang + ".svg",
+        "title": name,
+        "alt": name
+      });
+      var fileName = newElement("span", [newText("file." + ext)], ["file-name"]);
       var lines = text.split(splitRegex);
       var loc = lines.length;
       var sloc = lines.filter(x => trivialCodeRegex.test(x)).length;
       var statsText = loc === sloc ? loc + " LOC" : loc + " LOC (" + sloc + " SLOC)";
-      var stats = newElement("span", [newText(statsText)]);
-      stats.classList.add("stats");
-      var titleBar = newElement("div", [langIcon, fileName, stats, newClearFix()]);
-      titleBar.classList.add("title-bar");
-      var wrapper = newElement("div", [titleBar, preElement]);
-      wrapper.classList.add("snippet");
-      return wrapper;
+      var stats = newElement("span", [newText(statsText)], ["stats"]);
+      var titleBar = newElement("div", [langIcon, fileName, stats, newClearFix()], ["title-bar"]);
+      return newElement("div", [titleBar, preElement], ["snippet"]);
     }
 
     function processXml(xml) {
       if (xml.nodeType === xml.TEXT_NODE) {
         return xml;
+      } else if (xml.nodeName === "NOMOBILE") {
+        var div = newElement("div", xml.childNodes);
+        div.classList.add("no-mobile");
+        return div;
+      } else if (xml.nodeName === "ONLYMOBILE") {
+        var div = newElement("div", xml.childNodes);
+        div.classList.add("only-mobile");
+        return div;
       } else if (xml.nodeName === "C") {
         return newElement("code", _.map(xml.childNodes, processXml));
       } else if (xml.nodeName === "HTML5") {
@@ -142,7 +153,7 @@ define(["marked", "jquery", "mathjax", "hljs", "lodash"], function (marked, $, i
       } else if (xml.nodeName === "SCALA") {
         return codeHandler("scala", "scala", "Scala", xml);
       } else if (xml.nodeName === "TOC") {
-        return newElement("ul", _.map(xml.children, function (item) {
+        return newElement("ul", _.map(xml.children, item => {
           var a = newElement("a", [document.createTextNode(item.textContent)]);
           a.setAttribute("href", "/?" + item.attributes.url.value);
           return newElement("li", [a]);
@@ -167,14 +178,14 @@ define(["marked", "jquery", "mathjax", "hljs", "lodash"], function (marked, $, i
       $("table").wrap("<div class=\"table-wrapper\"></div>");
 
       // Navigate within the site using PushState
-      $("a").each(function () {
-        if (this.host === window.location.host) {
-          $(this).click(function (e) {
+      $("a").each(a => {
+        if (a.host === window.location.host) {
+          $(a).click(e => {
             if (e.which !== 1) {
               return true;
             }
 
-            var url = $(this).attr("href"),
+            var url = $(a).attr("href"),
               articleId = parseArticleId(url);
             history.pushState({articleId: articleId}, "", url);
             loadArticle(articleId);
@@ -185,7 +196,7 @@ define(["marked", "jquery", "mathjax", "hljs", "lodash"], function (marked, $, i
       });
 
       // Polymer introduces a delay in the rendering of the page
-      setTimeout(function () {
+      setTimeout(() => {
         hljs.initHighlighting.called = false;
         hljs.initHighlighting();
 
@@ -220,11 +231,11 @@ define(["marked", "jquery", "mathjax", "hljs", "lodash"], function (marked, $, i
           type: "GET",
           dataType: "text",
           url: articleUrl,
-          success: function (articleMarkdown) {
+          success: articleMarkdown => {
             rawMarkdowns[articleFile] = articleMarkdown;
             populateArticle(articleDiv, articleFile, articleMarkdown);
           },
-          error: function (ignore, textStatus, errorThrown) {
+          error: (ignore, textStatus, errorThrown) => {
             articleDiv.html("failed to load article content<br />" + textStatus + "<br />" + errorThrown);
           }
         });
@@ -235,7 +246,7 @@ define(["marked", "jquery", "mathjax", "hljs", "lodash"], function (marked, $, i
       } else {
         $.getJSON(
           getCommitHistoryUrl(articleUrl),
-          function (data) {
+          data => {
             var author, date;
 
             if (data.length > 0) {
@@ -250,7 +261,7 @@ define(["marked", "jquery", "mathjax", "hljs", "lodash"], function (marked, $, i
     }
 
     function init() {
-      $(window).on("popstate", function (event) {
+      $(window).on("popstate", event => {
         var articleId = event.originalEvent.state
           ? event.originalEvent.state.articleId
           : defaultArticle;
@@ -270,7 +281,7 @@ define(["marked", "jquery", "mathjax", "hljs", "lodash"], function (marked, $, i
       getPageTitle: getPageTitle,
       init: init
     };
-  }());
+  })();
 
   return main;
 });
