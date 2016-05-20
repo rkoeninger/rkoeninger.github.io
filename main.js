4a752e813d7cfe91b36eede063b6f6e3fa9d311d
rkoeninger <rkoeninger@att.net>
2016-05-19 21:38:35 -0400

    Added dos logo svg
    Converted commands.md to html

--- main.js
+++ main.js
@@ -158,6 +158,8 @@ define(["marked", "jquery", "mathjax", "hljs", "lodash"], (marked, $, ignore, hl
         return codeHandler("haskell", "Haskell", xml);
       } else if (xml.nodeName === "SCALA") {
         return codeHandler("scala", "Scala", xml);
+      } else if (xml.nodeName === "DOS") {
+        return codeHandler("dos", "Windows Batch", xml);
       } else if (xml.nodeName === "TOC") {
         return newElement("ul", _.map(xml.children, item => {
           var a = newElement("a", [document.createTextNode(item.textContent)]);
