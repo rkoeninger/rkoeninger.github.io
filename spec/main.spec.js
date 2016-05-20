85ec7f44605c34f5c40cb097d91a57f874b4f0c2
rkoeninger <rkoeninger@att.net>
2016-05-19 23:19:02 -0400

    Extended travis test delay to 20 seconds
    Converted functions in main.spec.js to arrow functions

--- spec/main.spec.js
+++ spec/main.spec.js
@@ -3,59 +3,59 @@
 /*jslint indent: 2*/
 /*global define, describe, it, expect*/
 
-define(["main"], function (main) {
-  describe("endsWith", function () {
-    it("endsWith(x + y, y) should always be true", function () {
+define(["main"], main => {
+  describe("endsWith", () => {
+    it("endsWith(x + y, y) should always be true", () => {
       expect(main.endsWith("abc" + "xyz", "xyz")).toBe(true);
     });
-    it("should always be true when suffix is the empty string", function () {
+    it("should always be true when suffix is the empty string", () => {
       expect(main.endsWith("abc", "")).toBe(true);
       expect(main.endsWith("", "")).toBe(true);
     });
   });
-  describe("contains", function () {
-    it("should never be true when list is empty", function () {
+  describe("contains", () => {
+    it("should never be true when list is empty", () => {
       expect(main.contains([], "abc")).toBe(false);
       expect(main.contains([], "")).toBe(false);
       expect(main.contains([], null)).toBe(false);
     });
-    it("should match anything that matches at least one item in the list", function () {
+    it("should match anything that matches at least one item in the list", () => {
       expect(main.contains(["abc", "xyz"], "xyz")).toBe(true);
     });
-    it("should match suffixes", function () {
+    it("should match suffixes", () => {
       expect(main.contains(["abc", "xyz"], "tuvwxyz")).toBe(true);
     });
-    it("should not match just any substring", function () {
+    it("should not match just any substring", () => {
       expect(main.contains(["abc", "xyz"], "asd_xyz_hds")).toBe(false);
     });
   });
-  describe("getArticleFileName", function () {
-    it("should return default article with extension for empty query string", function () {
+  describe("getArticleFileName", () => {
+    it("should return default article with extension for empty query string", () => {
       expect(main.getArticleFileName("")).toEqual("default.html");
     });
-    it("should return default article with extension for query string with no args", function () {
+    it("should return default article with extension for query string with no args", () => {
       expect(main.getArticleFileName("?")).toEqual("default.html");
     });
-    it("should return default article with extension for query string with no articleId arg", function () {
+    it("should return default article with extension for query string with no articleId arg", () => {
       expect(main.getArticleFileName("?abc=xyz&def=tuv")).toEqual("default.html");
     });
-    it("should treat articleId with no value just like missing articleId arg", function () {
+    it("should treat articleId with no value just like missing articleId arg", () => {
       expect(main.getArticleFileName("?articleId=")).toEqual("default.html");
     });
-    it("should find articleId no matter where it is in query string", function () {
+    it("should find articleId no matter where it is in query string", () => {
       expect(main.getArticleFileName("?articleId=abc.md")).toEqual("abc.md");
       expect(main.getArticleFileName("?abc=xyz&articleId=abc.md")).toEqual("abc.md");
       expect(main.getArticleFileName("?articleId=abc.md&def=tuv")).toEqual("abc.md");
       expect(main.getArticleFileName("?abc=xyz&articleId=abc.md&def=tuv")).toEqual("abc.md");
     });
-    it("should treat single a single query string arg with no '=value' as the articleId", function () {
+    it("should treat single a single query string arg with no '=value' as the articleId", () => {
       expect(main.getArticleFileName("?whatever.html")).toEqual("whatever.html");
     });
-    it("should add the .html extension if no other file extension", function () {
+    it("should add the .html extension if no other file extension", () => {
       expect(main.getArticleFileName("?articleId=whatever")).toEqual("whatever.html");
       expect(main.getArticleFileName("?whatever")).toEqual("whatever.html");
     });
-    it("should not add the .html extension if articleId has some other extension", function () {
+    it("should not add the .html extension if articleId has some other extension", () => {
       expect(main.getArticleFileName("?articleId=abc.md")).toEqual("abc.md");
     });
   });
